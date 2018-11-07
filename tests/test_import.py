daf981d8da552fb5a1780ca13392f90cbe226071
QU Xiaofeng <xiaofeng.qu@aqara.com>
2018-11-07 17:57:00 +0800

    test: add more tests for tox
    Signed-off-by: QU Xiaofeng <xiaofeng.qu@aqara.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,11 +3,20 @@
 
 def test_import_imread():
 	import os
-	from imreadeval import imread
+	import imreadeval
 	test_image_directory = os.path.join(os.path.dirname(__file__), '..', 'images')
-	filename = os.path.join(test_image_directory, "squirrel.jpg")
+	filename = os.path.join(test_image_directory, r'squirrel.jpg')
 	try:
-		im = imread(filename)
+		im = imreadeval.imread(filename)
+	except Exception as e:
+		assert False
+		return
+	assert True
+
+def test_import_imread_eval():
+	import imreadeval
+	try:
+		imreadeval.imread_eval()
 	except Exception as e:
 		assert False
 		return
