3c7801940aba619c36f2cc40f21765bf299ec60b
QU Xiaofeng <xiaofeng.qu@aqara.com>
2018-11-07 17:56:24 +0800

    fix: expose imread import function
    Signed-off-by: QU Xiaofeng <xiaofeng.qu@aqara.com>

--- imreadeval/__init__.py
+++ imreadeval/__init__.py
@@ -5,6 +5,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .imread import imread, imread_eval
+from .imread import imread, imread_eval, import_imread_from
 
 __all__ = ["imread", "imread_eval", "import_imread_from"]
